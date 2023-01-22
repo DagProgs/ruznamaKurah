92617154fb0b9fd117c747df2f14899625551a50
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-01-22 13:47:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-cache-v14';
-const dynamicCacheName = 'dynamic-cache-v14';
+const staticCacheName = 'static-cache-v15';
+const dynamicCacheName = 'dynamic-cache-v15';
 
 const staticAssets = [
   './',
