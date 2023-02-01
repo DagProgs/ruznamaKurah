56e18c3e3aace55e78cef6467a2d155aae2e0ac5
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-01 23:30:04 +0300

    ruznama

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-window.addEventListener('load', () => {
-
-    if ('serviceWorker' in navigator){
-
-        navigator.serviceWorker.register('sw.js')
-            .then(registration => {
-                console.log('Service worker successfully registered', registration);
-            })
-            .catch(error => {
-                console.log('Service worker registration failed', error);
-            });
+window.addEventListener('load', async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const reg = await navigator.serviceWorker.register('/sw.js')
+      console.log('Service worker register success', reg)
+    } catch (e) {
+      console.log('Service worker register fail')
     }
-});
+  }
+
+  await loadPosts()
+})
+
+
+async function loadPosts() {
+  const res = await fetch('')
+  const data = await res.json()
+
+  const container = document.querySelector('#posts')
+  container.innerHTML = data.map(toCard).join('\n')
+}
+
+function toCard(post) {
+  return `
+    <div class="card">
+      <div class="card-title">
+        ${post.title}
+      </div>
+      <div class="card-body">
+        ${post.body}
+      </div>
+    </div>
+  `
+}
