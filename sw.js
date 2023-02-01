02695c40d12f89fe5be8c099c93523d603a46096
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-02 00:05:40 +0300

    ruznama

--- sw.js
+++ sw.js
@@ -1,53 +1,93 @@
-const staticCacheName = 's-app-v36'
-const dynamicCacheName = 'd-app-v36'
 
-const assetUrls = [
-  'index.html',
-  '/js/app.js',
-  '/css/styles.css',
-  'offline.html'
-]
+const staticCacheName = 'static-cache-v0';
+const dynamicCacheName = 'dynamic-cache-v0';
+
+const staticAssets = [
+    './',
+    './index.html',
+	'./april.html',
+	'./august.html',
+	'./december.html',
+	'./february.html',
+	'./january.html',
+	'./july.html',
+	'./june.html',
+	'./march.html',
+	'./may.html',
+	'./november.html',
+	'./october.html',
+	'./september.html',
+    './images/icons/icon-128x128.png',
+    './images/icons/icon-192x192.png',
+    './offline.html',
+    './css/april.css',
+	'./css/august.css',
+	'./css/december.css',
+	'./css/february.css',
+	'./css/font-awesome.min.css',
+	'./css/index.css',
+	'./css/january.css',
+	'./css/july.css',
+	'./css/june.css',
+	'./css/march.css',
+	'./css/may.css',
+	'./css/november.css',
+	'./css/october.css',
+	'./css/september.css',
+	'./css/Untitled1.css',
+    './js/app.js',
+    './js/main.js',
+	'./js/jquery-3.6.0.min.js',
+	'./js/jquery-ui.min.js',
+	'./js/wb.cookie.min.js',
+	'./js/wb.panel.min.js',
+	'./js/wb.rotate.min.js',
+	'./js/wwb18.min.js',
+	'./db/ruznama_k.db',
+    './images/no-image.jpg'
+];
 
 self.addEventListener('install', async event => {
-  const cache = await caches.open(staticCacheName)
-  await cache.addAll(assetUrls)
-})
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(staticAssets);
+    console.log('Service worker has been installed');
+});
 
 self.addEventListener('activate', async event => {
-  const cacheNames = await caches.keys()
-  await Promise.all(
-    cacheNames
-      .filter(name => name !== staticCacheName)
-      .filter(name => name !== dynamicCacheName)
-      .map(name => caches.delete(name))
-  )
-})
+    const cachesKeys = await caches.keys();
+    const checkKeys = cachesKeys.map(async key => {
+        if (![staticCacheName, dynamicCacheName].includes(key)) {
+            await caches.delete(key);
+        }
+    });
+    await Promise.all(checkKeys);
+    console.log('Service worker has been activated');
+});
 
 self.addEventListener('fetch', event => {
-  const {request} = event
+    console.log(`Trying to fetch ${event.request.url}`);
+    event.respondWith(checkCache(event.request));
+});
 
-  const url = new URL(request.url)
-  if (url.origin === location.origin) {
-    event.respondWith(cacheFirst(request))
-  } else {
-    event.respondWith(networkFirst(request))
-  }
-})
-
-
-async function cacheFirst(request) {
-  const cached = await caches.match(request)
-  return cached ?? await fetch(request)
+async function checkCache(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || checkOnline(req);
 }
 
-async function networkFirst(request) {
-  const cache = await caches.open(dynamicCacheName)
-  try {
-    const response = await fetch(request)
-    await cache.put(request, response.clone())
-    return response
-  } catch (e) {
-    const cached = await cache.match(request)
-    return cached ?? await caches.match('/offline.html')
-  }
+async function checkOnline(req) {
+    const cache = await caches.open(dynamicCacheName);
+    try {
+        const res = await fetch(req);
+        await cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        const cachedRes = await cache.match(req);
+        if (cachedRes) {
+            return cachedRes;
+        } else if (req.url.indexOf('.html') !== -1) {
+            return caches.match('./offline.html');
+        } else {
+            return caches.match('./images/no-image.jpg');
+        }
+    }
 }
