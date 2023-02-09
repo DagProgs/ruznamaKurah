c0c8d68273eae12f1f1845eadef98fddc39b94e0
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-09 18:30:45 +0300

    Delete dataarru.js

--- js/dataarru.js
+++
@@ -1,9 +0,0 @@
-$('.my-date').hijriDate({
-            showGregDate: true,
-            showWeekDay: true,
-            separator: '&nbsp&nbsp|&nbsp&nbsp',
-            weekDayLang: 'ru',
-            gregLang: 'ru',
-            hijriLang: 'ru',
-            correction: +0
-        });
