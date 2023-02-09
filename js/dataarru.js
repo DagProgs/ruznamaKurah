36ebacc0d583a18bb13715bbfc08c05d4735e1fe
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-09 17:29:20 +0300

    Create dataarru.js

---
+++ js/dataarru.js
@@ -0,0 +1,9 @@
+$('.my-date').hijriDate({
+            showGregDate: true,
+            showWeekDay: true,
+            separator: '&nbsp&nbsp|&nbsp&nbsp',
+            weekDayLang: 'ru',
+            gregLang: 'ru',
+            hijriLang: 'ru',
+            correction: +0
+        });
