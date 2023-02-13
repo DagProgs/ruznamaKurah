cb234d0afd91bb6141b5c7277761d5fcfebf2619
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-13 09:19:31 +0300

    r

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-
-window.addEventListener('load', () => {
-
-    if ('serviceWorker' in navigator){
-
-        navigator.serviceWorker.register('./sw.js')
-            .then(registration => {
-                console.log('Service worker successfully registered', registration);
-            })
-            .catch(error => {
-                console.log('Service worker registration failed', error);
-            });
-    }
-});
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err));
+}
