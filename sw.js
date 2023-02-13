11afe26ccd8841352ecb4243a2cec59d386a01aa
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-13 07:57:20 +0300

    ruznama

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-
-const staticCacheName = 'static-kurahruznama-v3';
-const dynamicCacheName = 'dynamic-kurahruznama-v3';
-
-const staticAssets = [
-    './',
+//cache name
+const CACHE_NAME = "kurahruznama-v0";
+//we want to cache the next files
+const cacheAssets = [
+	'./',
     './index.html',
     './offline.html',
 	'./css/font-awesome.min.css',
@@ -19,48 +18,50 @@ const staticAssets = [
 	'./js/wwb18.min.js',
     './images/no-image.jpg'
 ];
-
-self.addEventListener('install', async event => {
-    const cache = await caches.open(staticCacheName);
-    await cache.addAll(staticAssets);
-    console.log('Service worker has been installed');
+//Install event
+self.addEventListener("install", e => {
+  console.log("Service Worker Installed");
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache => {
+        console.log("From service worker:caching files");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => {
+        self.skipWaiting();
+      })
+  );
+});
+//Active event
+self.addEventListener("activate", e => {
+  console.log("Service Worker activated");
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log("Cleaning up old cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener('activate', async event => {
-    const cachesKeys = await caches.keys();
-    const checkKeys = cachesKeys.map(async key => {
-        if (![staticCacheName, dynamicCacheName].includes(key)) {
-            await caches.delete(key);
-        }
-    });
-    await Promise.all(checkKeys);
-    console.log('Service worker has been activated');
-});
-
-self.addEventListener('fetch', event => {
-    console.log(`Trying to fetch ${event.request.url}`);
-    event.respondWith(checkCache(event.request));
-});
-
-async function checkCache(req) {
-    const cachedResponse = await caches.match(req);
-    return cachedResponse || checkOnline(req);
-}
-
-async function checkOnline(req) {
-    const cache = await caches.open(dynamicCacheName);
-    try {
-        const res = await fetch(req);
-        await cache.put(req, res.clone());
+//Fetch event
+self.addEventListener("fetch", e => {
+  console.log("fetching cached content");
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copyCache = res.clone();
+        caches.open(cacheName).then(cache => {
+          cache.put(e.request, copyCache);
+        });
         return res;
-    } catch (error) {
-        const cachedRes = await cache.match(req);
-        if (cachedRes) {
-            return cachedRes;
-        } else if (req.url.indexOf('.html') !== -1) {
-            return caches.match('./offline.html');
-        } else {
-            return caches.match('./images/no-image.jpg');
-        }
-    }
-}
+      })
+      .catch(error => caches.match(e.request).then(res => res))
+  );
+});
