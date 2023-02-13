6191e637f0154f37cfc9a1eeb1d6551ac24d0dcf
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-13 08:58:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,64 @@
+var urlsToCache = [
+	'./',
+    './index.html',
+    './offline.html',
+	'./css/font-awesome.min.css',
+	'./css/index.css',
+	'./css/RuznamaKurakh_end.css',
+    './js/app.js',
+    './js/main.js',
+	'./js/jquery.hijri.date.min.js',
+	'./js/jquery-3.6.0.min.js',
+	'./js/jquery-ui.min.js',
+	'./js/dayruznama.js',
+	'./js/script.js.js',
+	'./js/wwb18.min.js',
+    './images/no-image.jpg'
+];
 
-
-const cacheNameToKeep = 'kurahruznama-v2';
-
-//Deletion should only occur at the activate event
-self.addEventListener('activate', event => {
-    var cacheKeeplist = [cacheName];
-    event.waitUntil(
-        caches.keys().then( keyList => {
-            return Promise.all(keyList.map( key => {
-                if (cacheKeeplist.indexOf(key) === -1) {
-                    return caches.delete(key);
-                }
-            }));
-        })
-.then(self.clients.claim())); //this line is important in some contexts
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache')
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
+
+self.addEventListener('activate', event => {
+
+var validCaches = ['home-cache-v2', 'articles-cache-v2'];
+
+  event.waitUntil(
+    caches.keys().then(keys => 
+    Promise.all(keys.map(key => {
+        if (validCaches.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      // We successfully deleted all the obsolete caches
+    })
+  );
+});
+
+navigator.serviceWorker.getRegistration()
+   .then(function(registration) {
+       if(registration){
+          registration.unregister()
+          .then(
+               function(success) {
+                 if ('caches' in window) {
+    caches.keys()
+      .then(function(keyList) {
+          return Promise.all(keyList.map(function(key) {
+              return caches.delete(key);
+          }));
+      })
+}
+                });
+          }
+    });
+
+
+
