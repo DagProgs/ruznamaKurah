c08c307ca955667f6ce5978dab896566ee5d7ca0
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-13 08:47:54 +0300

    r

--- sw.js
+++ sw.js
@@ -1,24 +1,6 @@
 
 
-const cacheNameToKeep = 'kurahruznama-v0';
-
-const dynamicAssets = [
-    './',
-    './index.html',
-    './offline.html',
-	'./css/font-awesome.min.css',
-	'./css/index.css',
-	'./css/RuznamaKurakh_end.css',
-    './js/app.js',
-    './js/main.js',
-	'./js/jquery.hijri.date.min.js',
-	'./js/jquery-3.6.0.min.js',
-	'./js/jquery-ui.min.js',
-	'./js/dayruznama.js',
-	'./js/script.js.js',
-	'./js/wwb18.min.js',
-    './images/no-image.jpg'
-];
+const cacheNameToKeep = 'kurahruznama-v1';
 
 //Deletion should only occur at the activate event
 self.addEventListener('activate', event => {
