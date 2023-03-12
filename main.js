3eead86cc05918a8115b88d241ba5bfb7389f2fa
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-12 12:30:40 +0300

    upd

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('sw.js',{scope:'./'})
-  .then(succ=>{console.log(`registered ${succ}`)})
-.catch(err=>{console.log(`error ${err}`)})
-}else{console.log('browser not support')}
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => {
+      console.log(reg);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
