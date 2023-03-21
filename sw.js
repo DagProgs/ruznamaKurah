0bad2a37778255ae8c11e5337ef1328431d4679f
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-20 19:22:37 -0700

    upd

---
+++ sw.js
@@ -0,0 +1,51 @@
+//cache name
+const CACHE_NAME = "mullinstack.com-v2";
+//we want to cache the next files
+const cacheAssets = ["index.html", "offline.html", "js/main.js"];
+//Install event
+self.addEventListener("install", e => {
+  console.log("Service Worker Installed");
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache => {
+        console.log("From service worker:caching files");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => {
+        self.skipWaiting();
+      })
+  );
+});
+//Active event
+self.addEventListener("activate", e => {
+  console.log("Service Worker activated");
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log("Cleaning up old cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+//Fetch event
+self.addEventListener("fetch", e => {
+  console.log("fetching cached content");
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copyCache = res.clone();
+        caches.open(cacheName).then(cache => {
+          cache.put(e.request, copyCache);
+        });
+        return res;
+      })
+      .catch(error => caches.match(e.request).then(res => res))
+  );
+});
