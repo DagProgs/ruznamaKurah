988fccb4d3addd9e6cac53b93a4e2d7bcfeba7dc
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-22 14:12:38 +0300

    upd

---
+++ sw-workbox.js
@@ -0,0 +1,77 @@
+importScripts('workbox-v4.3.0/workbox-sw.js');
+
+// SETTINGS
+
+// Path prefix to load modules locally
+workbox.setConfig({
+  modulePathPrefix: 'workbox-v4.3.0/'
+});
+
+// Turn on logging
+workbox.setConfig({
+  debug: true
+});
+
+// Updating SW lifecycle to update the app after user triggered refresh
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+// PRECACHING
+
+// We inject manifest here using "workbox-build" in workbox-build-inject.js
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "index.html",
+    "revision": "b8fd299cea0e4ee38df86e75aeb6a46c"
+  },
+  {
+    "url": "css/style.css",
+    "revision": "365ad46ea4ecf2497897b116869a9a06"
+  },
+  {
+    "url": "js/accordion.js",
+    "revision": "7b6c7076f2a35632b36f49d6e7f2b099"
+  },
+  {
+    "url": "js/dayruznama.js",
+    "revision": "ac964b7f0da9833ef244841320d561a2"
+  },
+  {
+    "url": "js/jquery-3.6.0.min.js",
+    "revision": "0732e3eabbf8aa7ce7f69eedbd07dfdd"
+  }
+]);
+
+// RUNTIME CACHING
+
+// Google fonts
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ]
+  })
+);
+
+// API with network-first strategy
+workbox.routing.registerRoute(
+  /(http[s]?:\/\/)?([^\/\s]+\/)timeline/,
+  workbox.strategies.networkFirst()
+)
+
+// API with cache-first strategy
+workbox.routing.registerRoute(
+  /(http[s]?:\/\/)?([^\/\s]+\/)favorites/,
+  workbox.strategies.cacheFirst()
+)
+
+// OTHER EVENTS
+
+// Receive push and show a notification
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker]: Received push event', event);
+});
