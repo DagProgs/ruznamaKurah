545341f805bf57e0098db8122a79c2baef012afd
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-23 13:13:56 +0300

    upd

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-window.addEventListener('load', () => {
-			if ('serviceWorker' in navigator){
-				navigator.serviceWorker.register('./sw.js')
-					.then(registration => {
-					console.log('Service worker successfully registered', registration);
-            })
-				.catch(error => {
-					console.log('Service worker registration failed', error);
-            });
-			}
-		});
+
+  if (typeof navigator.serviceWorker !== 'undefined') {
+    navigator.serviceWorker.register('pwabuilder-sw.js')
+  }
