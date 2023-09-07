aba8b561f54475705e226a4154ed19f7c4f0952c
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-09-07 22:23:11 +0300

    Update sw-workbox.js

--- sw-workbox.js
+++ sw-workbox.js
@@ -69,56 +69,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "dfa2d81d118ae33a1cb63cdd9c20d3d2"
   },
   {
-    "url": "img/hiasan.png",
-    "revision": "68a642d1aa582d801540584db10abe19"
-  },
-  {
-    "url": "img/icons/icon-128x128.png",
-    "revision": "25c8eb241d5e0c913da717f6007736b2"
-  },
-  {
-    "url": "img/icons/icon-144x144.png",
-    "revision": "6e606e6871ccc1fdc7222dee1d72d42e"
-  },
-  {
-    "url": "img/icons/icon-152x152.png",
-    "revision": "33b8202ee77c28c332a4fa3efee61d34"
-  },
-  {
-    "url": "img/icons/icon-192x192.png",
-    "revision": "c5d401eb140c47f0d0a1b8880b5c8b49"
-  },
-  {
-    "url": "img/icons/icon-284x284.png",
-    "revision": "47f069d621e0e363d1f0b560be4335dc"
-  },
-  {
-    "url": "img/icons/icon-384x384.png",
-    "revision": "47f069d621e0e363d1f0b560be4335dc"
-  },
-  {
-    "url": "img/icons/icon-48x48.png",
-    "revision": "89428bb1e2be1296fb147effedac4a0f"
-  },
-  {
-    "url": "img/icons/icon-512x512.png",
-    "revision": "84f212482ada6ec3913a2a76d4b89c0d"
-  },
-  {
-    "url": "img/icons/icon-72x72.png",
-    "revision": "9c82c0475577731db0e52b9fa62e8c05"
-  },
-  {
-    "url": "img/icons/icon-96x96.png",
-    "revision": "9815fb3c4b57df1e8cda23d01fc66078"
-  },
-  {
-    "url": "img/rolling-dots.gif",
-    "revision": "7585284276e328d3bcdc46b262e89d5a"
-  },
-  {
-    "url": "img/update.svg",
-    "revision": "dd0041bacce10287c9794f63fb667e7d"
+    "url": "js/db/ruznama_k.db",
+    "revision": "de460c0d225b60cf38d9b38e3d99fdc8"
   }
 ]);
 
