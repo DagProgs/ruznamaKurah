447c937f48c9874fa567f8cc0ab58d9a5adf911a
DagProgs <114062397+DagProgs@users.noreply.github.com>
2024-04-18 13:34:15 +0300

    update

--- sw-workbox.js
+++ sw-workbox.js
@@ -1,13 +1,24 @@
 importScripts('./workbox-v4.3.1/workbox-sw.js');
 
+// SETTINGS
+
+// Path prefix to load modules locally
 workbox.setConfig({
-  modulePathPrefix: './workbox-v4.3.1/',
+  modulePathPrefix: './workbox-v4.3.1/'
+});
+
+// Turn on logging
+workbox.setConfig({
   debug: true
 });
 
+// Updating SW lifecycle to update the app after user triggered refresh
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
+// PRECACHING
+
+// We inject manifest here using "workbox-build" in workbox-build-inject.js
 workbox.precaching.precacheAndRoute([
   {
     "url": "index.html",
@@ -159,6 +170,8 @@ workbox.precaching.precacheAndRoute([
   }
 ]);
 
+// RUNTIME CACHING
+
 // Google fonts
 workbox.routing.registerRoute(
   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
@@ -172,28 +185,21 @@ workbox.routing.registerRoute(
   })
 );
 
+// API with network-first strategy
 workbox.routing.registerRoute(
   /(http[s]?:\/\/)?([^\/\s]+\/)timeline/,
   workbox.strategies.networkFirst()
-);
+)
 
+// API with cache-first strategy
 workbox.routing.registerRoute(
   /(http[s]?:\/\/)?([^\/\s]+\/)favorites/,
   workbox.strategies.cacheFirst()
-);
+)
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
+// OTHER EVENTS
 
+// Receive push and show a notification
 self.addEventListener('push', function(event) {
   console.log('[Service Worker]: Received push event', event);
 });
